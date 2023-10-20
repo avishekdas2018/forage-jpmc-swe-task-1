20f76f7fb88d0e9dabc8c2865674d5efe387e29a
Avishek Das <dasavishek2013@gmail.com>
2023-10-20 19:55:54 +0530

    bonus task added

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,40 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+  class ClientTest(unittest.TestCase):
+    def test_getRatio_PriceBZero(self):
+        price_a = 10.0
+        price_b = 0.0
+        expected_result = "Cannot divide by Zero"
+        self.assertEqual(getRatio(price_a, price_b), expected_result)
+
+    def test_getRatio_PriceAZero(self):
+        price_a = 0.0
+        price_b = 20.0
+        expected_result = 0.0
+        self.assertEqual(getRatio(price_a, price_b), expected_result)
+
+    def test_getRatio_PriceAAndBZero(self):
+        price_a = 0.0
+        price_b = 0.0
+        expected_result = "Cannot divide by Zero"
+        self.assertEqual(getRatio(price_a, price_b), expected_result)
+
+    def test_getRatio_NormalCase(self):
+        price_a = 15.0
+        price_b = 5.0
+        expected_result = 3.0
+        self.assertEqual(getRatio(price_a, price_b), expected_result)
+
+
+
+
+
 
 
 
